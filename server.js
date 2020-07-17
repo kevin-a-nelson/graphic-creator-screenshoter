1b2f493d9d11aabab301d2bfb6acb127cfafc407
SamuraiViking <nelson67@stolaf.edu>
2020-07-17 11:59:02 -0500

    cors

--- server.js
+++ server.js
@@ -13,11 +13,43 @@ const router = require('./router.js');
 //     next();
 // })
 
+app.use(function(req, res, next) {
+    var allowedOrigins = [
+      "http://localhost:3000"
+    ];
+    var origin = req.headers.origin;
+    console.log(origin)
+    console.log(allowedOrigins.indexOf(origin) > -1)
+    // Website you wish to allow to
+    if (allowedOrigins.indexOf(origin) > -1) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    
+    // res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+    
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type,Authorization"
+    );
+    
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    
+    // Pass to next layer of middleware
+    next();
+});
 
-
-app.use(cors(
-    { origin: '*'}
-))
+// app.use(cors(
+//     { origin: '*'}
+// ))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
