90be2e792f08bc467019947c718c4750e00e3d76
SamuraiViking <nelson67@stolaf.edu>
2020-07-17 08:37:29 -0500

    add puppeteer buildpack

--- router.js
+++ router.js
@@ -4,14 +4,19 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 
-
 router.get("/screenshots/:display/:texts", cors(),async (req, res) => {
 
     const display = req.params.display
     const texts = req.params.texts
     const url = `http://localhost:3000/?texts=${texts}&display=${display}`
 
-    const browser = await puppeteer.launch(); const page = await browser.newPage();
+    const browser = await puppeteer.launch({
+    'args' : [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+    ]
+    });
+    const page = await browser.newPage();
     await page.goto(url); // URL is given by the "user" (your client-side application)
     await page.setViewport({ width: 1300, height: 512 });
 
@@ -26,6 +31,7 @@ router.get("/screenshots/:display/:texts", cors(),async (req, res) => {
         }
     });
     
+
     await browser.close();
     res.json({
         "ImageString": screenshotBuffer
